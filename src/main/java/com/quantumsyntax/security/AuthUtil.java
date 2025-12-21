4fed1a7fa152d29dd5c6b2e2b2eb14e4c683bbdf
Rajneesh Kumar <192125aryaraj@gmail.com>
2025-12-21 22:50:17 +0530

    Add filters custom filters

--- src/main/java/com/quantumsyntax/security/AuthUtil.java
+++ src/main/java/com/quantumsyntax/security/AuthUtil.java
@@ -1,6 +1,7 @@
 package com.quantumsyntax.security;
 
 import com.quantumsyntax.model.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,5 +28,15 @@ public class AuthUtil {
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*10))
                 .signWith(getSecretKey())
                 .compact();
+    }
+
+    public String getUsernameFromToken(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
     }
 }
--- src/main/java/com/quantumsyntax/security/WebSecurityConfig.java
+++ src/main/java/com/quantumsyntax/security/WebSecurityConfig.java
@@ -8,11 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final PasswordEncoder passwordEncoder;
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
@@ -21,9 +23,12 @@ public class WebSecurityConfig {
                         sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/public/**", "/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
-                );
+                                .anyRequest().authenticated()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
         return httpSecurity.build();
     }
 
